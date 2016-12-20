c024426642ba1e457bb081c50ebbceca262ee610
Gabriel Alexandre <gabriel.shaad@gmail.com>
2016-12-20 17:07:02 -0200

    Criar calculadora Simples Nacional

--- app.js
+++ app.js
@@ -2,35 +2,86 @@
   'use strict';
 
   angular.module('app', [])
-  .controller('ListarAtividades', ListarAtividades)
+  .controller('CalcularSimplesNacional', CalcularSimplesNacional)
   .service('AtividadesService', AtividadesService)
+  .service('SimplesNacionalService', SimplesNacionalService)
+  // .service('LucroPresumidoService', LucroPresumidoService)
   .constant('ApiBasePath', "https://app-dot-contabilizei-jobs.appspot.com");
 
-  ListarAtividades.$inject = ['AtividadesService'];
-  function ListarAtividades(AtividadesService) {
-    var lista = this;
+  // Controller responsável pelo cálculo de imposto Simples Nacional
+  CalcularSimplesNacional.$inject = ['SimplesNacionalService', 'AtividadesService'];
+  function CalcularSimplesNacional(SimplesNacionalService, AtividadesService) {
+    var simples = this;
 
-    var promise = AtividadesService.getAtividades();
+    simples.fatAnterior   = 0.0;
+    simples.faturamento   = 0.0;
+    simples.folha         = 0.0;
+    simples.codAtividade  = 0;
+    simples.descAtividade = "";
 
-    promise.then(function (response) {
-      lista.atividades = response.data;
-      lista.descricaoAtividades = lista.atividades['objects'].map(function (o) {
+    // Resolver promessa do serviço de atividades
+    var promiseAtividades = AtividadesService.getAtividades();
+
+    promiseAtividades.then(function (response) {
+      simples.atividades = response.data;
+      simples.descricaoAtividades = simples.atividades['objects'].map(function (o) {
         return o.descricao;
       })
     })
     .catch(function (error) {
       console.log('Oops');
     });
+
+    // Calcular o valor total do imposto Simples Nacional
+    simples.calcularImpostoSimples = function (descAtividade) {
+      simples.descIndex = simples.descricaoAtividades.indexOf(descAtividade);
+      simples.codAtividade = simples.atividades['objects'].map(function (c) {
+        return c.cod;
+      });
+      simples.codAtividade = simples.codAtividade[simples.descIndex];
+
+      // Resolver promessa do Cálculo de Simples Nacional
+      var promiseSimples = SimplesNacionalService.getSimplesNacional(simples.fatAnterior, simples.faturamento, simples.folha, simples.codAtividade);
+
+      promiseSimples.then(function (response) {
+        simples.resultados = response.data;
+        simples.impostoSimples = simples.resultados['objects'].map(function (r) {
+          return r;
+        });
+        console.log(simples.impostoSimples);
+      })
+      .catch(function (error) {
+        console.log('Oops, não pude processar os dados do Simples Nacional.');
+      });
+    };
   }
 
+  // Service de requisição da lista de atividades e respectivos códigos
   AtividadesService.$inject = ['$http', 'ApiBasePath'];
   function AtividadesService($http, ApiBasePath) {
     var service = this;
 
+    // Request de atividades
     service.getAtividades = function () {
       var response = $http({
         method: "GET",
         url: (ApiBasePath + "/rest/simulador/atividades")
+      });
+      return response;
+    };
+  }
+
+  // Service de requisição dos valores do Simples Nacional
+  SimplesNacionalService.$inject = ['$http', 'ApiBasePath'];
+  function SimplesNacionalService($http, ApiBasePath) {
+    var service = this;
+
+    // Request e envio de parâmetros para o serviço de cálculo do Simples Nacional
+    service.getSimplesNacional = function (fatAnterior, faturamento, folha, codAtividade) {
+      var response = $http({
+        method: "GET",
+        url: (ApiBasePath + "/rest/simulador/imposto/simples"),
+        params: {fatanterior: fatAnterior, faturamento: faturamento, folha: folha, codatividade: codAtividade}
       });
       return response;
     };
